98e82cc52a725a7e587db8b8281aabce859377c0
freQuensy23-coder <mametevalex@gmail.com>
2022-07-29 00:40:16 +0300

    Вынес тексты в отдельный файл

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from bot_utils import *
 from aiogram import dispatcher
 import config
 from states import Registration, Menu
+from texts import Texts
 
 storage = MemoryStorage()
 bot = aiogram.Bot(token=config.TELEGRAM_TOKEN)
@@ -50,8 +51,8 @@ def restart_keyboard():
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: aiogram.types.Message):
-    await send_message(message.from_id, "Что бы зарегестрироваться выберете тот курс, который вы проходите", bot)
-    await message.reply("Выберете курс из списка на клавиатуре", reply_markup=get_courses_keyboard())
+    await send_message(message.from_id, Texts.start_1, bot)
+    await message.reply(Texts.start_select_course, reply_markup=get_courses_keyboard())
     await Registration.select_course.set()
 
 
@@ -59,11 +60,11 @@ async def cmd_start(message: aiogram.types.Message):
 async def course_selected(message: aiogram.types.Message, state):
     # TODO Save user course
     if message.text in config.courses:
-        await send_message(message.from_id, "Вы успешно зарегестрировались как участник курса: " + message.text, bot)
-        await message.reply("Для управление ботом можно использовать это меню", reply_markup=get_menu_keyboard())
+        await send_message(message.from_id, Texts.registration_succes + message.text, bot)
+        await message.reply(Texts.use_menu_help, reply_markup=get_menu_keyboard())
         await Menu.in_menu.set()
     else:
-        await send_message(message.from_id, "Вы ввели неверный курс", bot)
+        await send_message(message.from_id, Texts.no_such_course, bot)
 
 
 @dp.message_handler(commands=['i_am_administrator'], state='*')
@@ -77,12 +78,12 @@ async def cmd_pair_call(message: aiogram.types.Message, state):
     global waiting_for_pair_call  # TODO тут может быть какая то хуйня с асинхронностью
     if message.from_user == waiting_for_pair_call:
         await send_message(message.from_id,
-                           "Вы уже отправляли запрос на парный созвон. Что бы выйте из очереди напишите /kick_call",
+                           Texts.pair_call_already_created,
                            bot)
     else:
         waiting_for_pair_call = message.from_user
-        await send_message(message.from_id, "Вы отправили запрос на парный созвон. Ожидайте ответа", bot)
-        await message.reply("Выйти из очереди на парный созвон можно написав /kick_call")
+        await send_message(message.from_id, Texts.pair_call_created_success, bot)
+        await message.reply(Texts.leave_call_queue_help)
 
 
 @dp.message_handler(commands=['kick_call'], state='*')
@@ -90,32 +91,31 @@ async def cmd_kick_call(message: aiogram.types.Message, state):
     global waiting_for_pair_call
     if message.from_user == waiting_for_pair_call:
         waiting_for_pair_call = None
-        await send_message(message.from_id, "Вы успешно выйти из очереди на парный созвон", bot)
+        await send_message(message.from_id, Texts.leave_call_queue_success, bot)
     else:
-        await send_message(message.from_id, "Вы не находитесь в очереди на парный созвон", bot)
+        await send_message(message.from_id, Texts.you_are_not_in_queue, bot)
 
 
 @dp.message_handler(text=config.menu_notification, state=Menu.in_menu)
 async def cmd_notification(message: aiogram.types.Message, state):
-    await message.reply("Вы можете настроить уведомления о событиях из этого меню (пока не функционально)",
-                        reply_markup=get_notification_keyboard(message.from_user))
+    await message.reply(Texts.notification_help, reply_markup=get_notification_keyboard(message.from_user))
 
 
 @dp.message_handler(text="◀ В меню", state="*")
 async def force_menu(message, state):
-    await message.reply("Вы вернулись в меню", reply_markup=get_menu_keyboard())
+    await message.reply(Texts.use_menu_help, reply_markup=get_menu_keyboard())
     await Menu.in_menu.set()
 
 
 @dp.message_handler(text=config.menu_settings, state=Menu.in_menu)
 async def cmd_settings(message: aiogram.types.Message, state):
-    await message.reply("Вы можете изменить ваш курс", reply_markup=restart_keyboard())
+    await message.reply(Texts.settings_help, reply_markup=restart_keyboard())
     await Menu.in_settings.set()
 
 
 @dp.message_handler(text=config.settings_restart, state=Menu.in_settings)
 async def cmd_restart(message: aiogram.types.Message, state):
-    await send_message(message.from_id, "Вы вернулись к меню выбора курса", bot)
+    await send_message(message.from_id, Texts.select_course_again, bot)
     await Registration.select_course.set()
     await cmd_start(message)
 
