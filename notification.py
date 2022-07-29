c6c9f1b8a915abdcf81012bf0aaf1d9e7d5866ab
freQuensy23-coder <mametevalex@gmail.com>
2022-07-30 00:53:51 +0300

    Класс для хранения списка уведомлений

--- notification.py
+++ notification.py
@@ -8,3 +8,14 @@ class Notification:
     message: str
     time: datetime
     important_level: int
+
+
+class SimpleNotificationSender:
+    def __init__(self):
+        self.notifications = []
+
+    def add_notification(self, notification):
+        self.notifications.append(notification)
+
+    def get_notification(self):
+        return self.notifications
