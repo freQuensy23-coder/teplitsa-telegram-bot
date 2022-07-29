0a8c7382a20c1266a24f7345718d0e4868f007a9
freQuensy23-coder <mametevalex@gmail.com>
2022-07-30 01:54:55 +0300

    Вынес логику работы с клавиатурами в bot_utils.py

--- bot_utils.py
+++ bot_utils.py
@@ -1,7 +1,10 @@
 import asyncio
 
+import aiogram
 from aiogram import exceptions
 from logging import getLogger
+
+import config
 
 log = getLogger(__name__)
 
@@ -34,3 +37,46 @@ async def send_message(user_id: int, text: str, bot, disable_notification: bool 
         log.info(f"Target [ID:{user_id}]: success")
         return True
     return False
+
+
+def get_user_settings(from_user):  # TODO
+    pass
+
+
+def get_courses_keyboard():  # TODO
+    # Get keyboard with buttons from config.courses
+    keyboard = aiogram.types.ReplyKeyboardMarkup()
+    for course in config.courses:
+        keyboard.add(aiogram.types.KeyboardButton(course))
+    return keyboard
+
+
+def get_menu_keyboard():
+    keyboard = aiogram.types.ReplyKeyboardMarkup()
+    keyboard.add(aiogram.types.KeyboardButton(config.menu_settings))
+    keyboard.add(aiogram.types.KeyboardButton(config.menu_notification))
+    keyboard.add(aiogram.types.KeyboardButton(config.menu_pair_call))
+    return keyboard
+
+
+def get_notification_keyboard(from_user):
+    keyboard = aiogram.types.ReplyKeyboardMarkup()
+    user_settings = get_user_settings(from_user)
+    keyboard.add(aiogram.types.KeyboardButton("✅ Уведомлять о важный событиях"))
+    keyboard.add(aiogram.types.KeyboardButton("✅ Уведомлять о регулярных событиях"))
+    keyboard.add(aiogram.types.KeyboardButton("❌ Отправлять дополнительную информацию о событияъ событиях"))
+    keyboard.add(aiogram.types.KeyboardButton("◀ В меню"))
+    return keyboard
+
+
+def restart_keyboard():
+    keyboard = aiogram.types.ReplyKeyboardMarkup()
+    keyboard.add(aiogram.types.KeyboardButton(config.settings_restart))
+    return keyboard
+
+
+async def close(loop, dp, bot):
+    loop.stop()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    await bot.session.close()
