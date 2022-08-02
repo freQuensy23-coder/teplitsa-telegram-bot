3a5c6034b5d628371f620b6474dae177998c0504
freQuensy23-coder <mametevalex@gmail.com>
2022-08-02 23:12:04 +0300

    Update aync db sqlalchemy

--- db.py
+++ db.py
@@ -6,24 +6,26 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 Base = declarative_base()
 engine = create_async_engine('sqlite+aiosqlite:///test.sqlite3', echo=True)
 
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     telegram_id = Column(Integer)
     course = Column(String)
-    notification_mode = Column(Integer)  # 0 - all notifications, 1 - important, 2 - very important
+    notification_mode = Column(Integer, default=1)  # 0 - all notifications, 1 - important, 2 - very important
 
 
-async def get_or_create(async_sess, model, **kwargs):
+async def get_or_create(async_sess, model, commit=True, **kwargs):
     async with async_sess() as sess:
         async with sess.begin():
             instance = await sess.execute(select(model).filter_by(**kwargs))
-            if instance:
-                return instance
+            if instance.first():
+                return instance.first()
             else:
                 instance = model(**kwargs)
                 sess.add(instance)
-                sess.commit()
+                if commit:
+                    await sess.commit()
                 return instance
 
 
